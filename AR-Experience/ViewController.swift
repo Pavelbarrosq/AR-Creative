7a296a52c4e8cef053b7ae3e9ee4340955afd28d
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-11-06 13:20:43 +0100

    Changed to creating floor with anchor, added debug options.

--- AR-Experience/ViewController.swift
+++ AR-Experience/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 import ARKit
 import SceneKit
 
-enum BodyType: Int {
-    case box = 1
-    case floor = 2
-}
+//enum BodyType: Int {
+//    case box = 1
+//    case floor = 2
+//}
 
 class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
 
@@ -25,19 +25,18 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         // Do any additional setup after loading the view.
         configuration.planeDetection = .horizontal
         arView.session.run(configuration, options: [])
-        arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        arView.debugOptions = [.showFeaturePoints, .showPhysicsShapes, .showPhysicsFields ]
         arView.autoenablesDefaultLighting = true
         arView.delegate = self
         arView.scene.physicsWorld.contactDelegate = self
-        
-        setDotMarker()
-        addFloor()
         
     }
     
     @IBAction func addButtonPressed(_ sender: UIButton) {
        
         addCube()
+//        arView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+
     }
     
     
@@ -51,134 +50,131 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         removeNodes()
     }
     
+    
+    
+
+    
+    func getCameraPos(sceneView: ARSCNView) -> Extensions.myCameraPos {
+        let cameraTransform = arView.session.currentFrame?.camera.transform
+        let cameraPos = MDLTransform(matrix: cameraTransform!)
+        
+        var cp = Extensions.myCameraPos()
+        cp.x = cameraPos.translation.x
+        cp.y = cameraPos.translation.y
+        cp.z = cameraPos.translation.z
+        
+        return cp
+    }
+    
+    func addCube() {
+        
+        let cubeNode = SCNNode()
+        cubeNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.002)
+        cubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+        let CamPos = getCameraPos(sceneView: arView)
+        cubeNode.position = SCNVector3(CamPos.x, CamPos.y, CamPos.z)
+        cubeNode.name = "cube"
+        cubeNode.physicsBody?.restitution = 0.8
+        cubeNode.physicsBody?.mass = 0.5
+        let cubeBodyShape = SCNPhysicsShape(geometry: cubeNode.geometry!, options: [SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.boundingBox])
+        cubeNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: cubeBodyShape)
+        cubeNode.physicsBody?.categoryBitMask = Extensions.BodyType.box.rawValue
+        cubeNode.physicsBody?.collisionBitMask = Extensions.BodyType.floor.rawValue | Extensions.BodyType.box.rawValue
+        cubeNode.physicsBody?.contactTestBitMask = Extensions.BodyType.floor.rawValue | Extensions.BodyType.box.rawValue
+        
+        arView.scene.rootNode.addChildNode(cubeNode)
+    }
+
+//    func addFloor() {
+//        let floorNode = SCNNode()
+//        floorNode.geometry = SCNFloor()
+//        floorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.8)
+//        floorNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.static, shape: SCNPhysicsShape(geometry: SCNFloor(), options: nil))
+//        floorNode.position = SCNVector3(0, -0.7, -0.5)
+//        arView.scene.rootNode.addChildNode(floorNode)
+//        floorNode.physicsBody?.categoryBitMask = Extensions.BodyType.floor.rawValue
+//
+//    }
+          
+    func createFloor(anchor: ARPlaneAnchor) -> SCNNode {
+        let floor = SCNNode()
+        let radian = deg2rad(90)
+        floor.name = "floor"
+        floor.eulerAngles = SCNVector3(radian, 0, 0)
+        floor.geometry = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        floor.geometry?.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.8)
+        floor.geometry?.firstMaterial?.isDoubleSided = true
+        floor.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
+        let floorBodyShape = SCNPhysicsShape(geometry: floor.geometry!, options: [SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.boundingBox])
+        floor.physicsBody = SCNPhysicsBody(type: .static, shape: floorBodyShape)
+        floor.physicsBody?.categoryBitMask = Extensions.BodyType.floor.rawValue
+
+        return floor
+    }
+    
+    func resetScene() {
+        arView.session.pause()
+        removeNodeWithString(named: "cube")
+        removeNodeWithString(named: "floor")
+        arView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
+//        addFloor()
+    }
+    
+    func removeNodes() {
+        removeNodeWithString(named: "cube")
+    }
+    
+    func removeNodeWithString(named: String) {
+        arView.scene.rootNode.enumerateChildNodes { (node, _) in
+            if node.name == named {
+                node.removeFromParentNode()
+            }
+        }
+    }
+    
+    func deg2rad(_ number: Double) -> Double {
+        return number * .pi / 180
+    }
+    
+    
+    //    Delegate
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let anchorPlane = anchor as? ARPlaneAnchor else {return}
+        print("New Planeanchor found with extent \(anchorPlane.extent)")
+
+        let floor = createFloor(anchor: anchorPlane)
+        node.addChildNode(floor)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let anchorPlane = anchor as? ARPlaneAnchor else {return}
+        print("Planeanchor updated with extent \(anchorPlane.extent)")
+        removeNodeWithString(named: "floor")
+        let floor = createFloor(anchor: anchorPlane)
+        node.addChildNode(floor)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let anchorPlane = anchor as? ARPlaneAnchor else {return}
+        print("Planeanchor removed with extent \(anchorPlane.extent)")
+        removeNodeWithString(named: "floor")
+    }
+    
+    // on contact
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
         print("Contact happened!")
     }
     
+    //Deletes node if it is under 2.5 meters down
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         self.arView.scene.rootNode.enumerateChildNodes { (nodes, _) in
             if (nodes.name == "cube") {
-//                print(nodes.presentation.position.y)
-                if (nodes.presentation.position.y < -1.7) {
+                if (nodes.presentation.position.y < -2.5) {
                     nodes.removeFromParentNode()
                 }
             }
         }
     }
-    
-    // RENDERER
-//    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-//        guard let anchorPlane = anchor as? ARPlaneAnchor else {return}
-//        print("New Planeanchor found with extent \(anchorPlane.extent)")
-//
-//        let floor = createFloor(anchor: anchorPlane)
-//        node.addChildNode(floor)
-//    }
-//
-//    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-//        guard let anchorPlane = anchor as? ARPlaneAnchor else {return}
-//        print("Planeanchor updated with extent \(anchorPlane.extent)")
-//        removeNodeWithString(named: "floor")
-//        let floor = createFloor(anchor: anchorPlane)
-//        node.addChildNode(floor)
-//    }
-//
-//    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-//        guard let anchorPlane = anchor as? ARPlaneAnchor else {return}
-//        print("Planeanchor removed with extent \(anchorPlane.extent)")
-//        removeNodeWithString(named: "floor")
-//    }
-    
-    
-    func addCube() {
-        let cubeNode = SCNNode()
-        cubeNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.002)
-        cubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
-        
-        cubeNode.position = SCNVector3(0, 0, -0.5)
-        
-        cubeNode.name = "cube"
-        arView.scene.rootNode.addChildNode(cubeNode)
-        
-        cubeNode.physicsBody?.restitution = 0.8
-        cubeNode.physicsBody?.mass = 0.5
-        cubeNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: cubeNode))
-        cubeNode.physicsBody?.categoryBitMask = BodyType.box.rawValue
-        cubeNode.physicsBody?.collisionBitMask = BodyType.floor.rawValue | BodyType.box.rawValue
-        cubeNode.physicsBody?.contactTestBitMask = BodyType.floor.rawValue | BodyType.box.rawValue
-        
-    }
-    
-    func setDotMarker(){
-        let dotNode = SCNNode()
-        dotNode.geometry = SCNSphere(radius: 0.015)
-        dotNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
-        dotNode.position = SCNVector3(0, 0, -0.5)
-        arView.scene.rootNode.addChildNode(dotNode)
-        
-    }
-    
-    
-    func addFloor() {
-        let floorNode = SCNNode()
-        floorNode.geometry = SCNFloor()
-        floorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.8)
-        floorNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.static, shape: SCNPhysicsShape(geometry: SCNFloor(), options: nil))
-        floorNode.position = SCNVector3(0, -0.7, -0.5)
-        arView.scene.rootNode.addChildNode(floorNode)
-        floorNode.physicsBody?.categoryBitMask = BodyType.floor.rawValue
-        
-    }
-          
-//    func createFloor(anchor: ARPlaneAnchor) -> SCNNode {
-//        let floor = SCNNode()
-//        let radian = deg2rad(90)
-//        floor.name = "floor"
-//        floor.eulerAngles = SCNVector3(radian, 0, 0)
-//        floor.geometry = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
-//        floor.geometry?.firstMaterial?.diffuse.contents = UIColor.clear
-//        floor.geometry?.firstMaterial?.isDoubleSided = true
-//        floor.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
-//        floor.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: floor))
-//        floor.physicsBody?.categoryBitMask = BodyType.floor.rawValue
-//        floor.physicsBody?.collisionBitMask = BodyType.box.rawValue
-//        floor.physicsBody?.contactTestBitMask = BodyType.box.rawValue
-//
-//
-//        return floor
-//    }
-    
-    func resetScene() {
-        arView.session.pause()
-        arView.scene.rootNode.enumerateChildNodes { (nodes, _) in
-            if nodes.name == "cube" || nodes.name == "floor" {
-                nodes.removeFromParentNode()
-            }
-        }
-        arView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
-        addFloor()
-    }
-    
-    func removeNodes() {
-        arView.scene.rootNode.enumerateChildNodes { (nodes, _) in
-            if nodes.name == "cube" {
-                nodes.removeFromParentNode()
-            }
-        }
-    }
-    
-//    func removeNodeWithString(named: String) {
-//        arView.scene.rootNode.enumerateChildNodes { (node, _) in
-//            if node.name == named {
-//                node.removeFromParentNode()
-//            }
-//        }
-//    }
-    
-//    func deg2rad(_ number: Double) -> Double {
-//        return number * .pi / 180
-//    }
-    
 
 }
 
